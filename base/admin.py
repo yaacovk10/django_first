60a861b15a0cf1d2f46ae4437c2c82d2bb63be2a
ShayCohenn <shay91847@gmail.com>
2023-10-23 15:49:38 +0300

    last update from django start

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Student
+from .models import Book
+
+admin.site.register(Student)
+admin.site.register(Book)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+from django.db import models
+
+
+# Create your models here.
+class Student(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    sName = models.CharField(max_length=20)
+    age = models.FloatField()
+
+
+    def __str__(self):
+        return self.sName
+    
+
+class Book(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.title
